5a0bdeb4d9ba39c0d1b1c21361725c936a5a0cd0
Ashadullah Shawon <shawonashadullah@gmail.com>
2020-08-04 18:24:16 +0600

    Update customer.component.ts

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CustomerService } from '../shared/customer.service';
 @Component({
   selector: 'app-customer',
-  templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+  templateUrl: 'customer.component.html'
 })
 export class CustomerComponent implements OnInit {
 
